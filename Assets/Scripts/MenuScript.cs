c9daf07a73c20af2b22cf1e3a335764c6d220336
Tim <ray.timothy@knights.ucf.edu>
2021-01-31 12:39:50 -0500

    Second Commit

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject menuPanel;
+    public GameObject aboutPanel;
+    public GameObject exitPanel;
+
+
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Level1-1");
+    }
+
+    public void About()
+    {
+        menuPanel.SetActive(false);
+        aboutPanel.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        menuPanel.SetActive(true);
+        aboutPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        menuPanel.SetActive(false);
+        exitPanel.SetActive(true);
+    }
+
+    public void ConfirmYes()
+    {
+        Application.Quit();
+    }
+
+    public void ConfirmNo()
+    {
+        exitPanel.SetActive(false);
+        menuPanel.SetActive(true);
     }
 }
